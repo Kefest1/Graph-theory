d7f6520e8f03026f173fff2fa5e1ba4c16129665
SzymonBig <szymonbig01@gmail.com>
2022-04-16 22:07:45 +0200

    Added main tests to debug.

--- Main.java
+++ Main.java
@@ -2,6 +2,11 @@ package com.company;
 
 class Main {
     public static void main(String[] args) {
+        UndirectedGraph graph = new UndirectedGraph(new Vertex_t('A'));
+        graph.addNode(new Vertex_t('B'));
+        graph.addNode(new Vertex_t('C'));
 
+        graph.addEdge(new Vertex_t('B'), new Vertex_t('C'));
+        graph.addEdge(new Vertex_t('A'), new Vertex_t('C'));
     }
 }
